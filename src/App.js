929d74bdd5b8e29c810a17222a199f716a27499b
afridarn <afridarohmatin@gmail.com>
2023-06-06 19:50:17 +0700

    change list and update form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    title: "Senka Facial Wash",
+    amount: "44000",
+    date: new Date(2023, 5, 23),
+  },
+  {
+    id: 2,
+    title: "Taiyaki",
+    amount: "35000",
+    date: new Date(2023, 5, 24),
+  },
+  {
+    id: 3,
+    title: "MR DIY",
+    amount: "16500",
+    date: new Date(2022, 5, 25),
+  },
+  {
+    id: 4,
+    title: "Choco Sundae",
+    amount: "12000",
+    date: new Date(2021, 5, 26),
+  },
+  {
+    id: 5,
+    title: "Car Park",
+    amount: "10000",
+    date: new Date(2020, 5, 27),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 1,
-      title: "Senka Facial Wash",
-      amount: "44000",
-      date: new Date(2023, 5, 23),
-    },
-    {
-      id: 2,
-      title: "Taiyaki",
-      amount: "35000",
-      date: new Date(2023, 5, 24),
-    },
-    {
-      id: 3,
-      title: "MR DIY",
-      amount: "16500",
-      date: new Date(2023, 5, 25),
-    },
-    {
-      id: 4,
-      title: "Choco Sundae",
-      amount: "12000",
-      date: new Date(2023, 5, 26),
-    },
-    {
-      id: 5,
-      title: "Car Park",
-      amount: "10000",
-      date: new Date(2023, 5, 27),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
       <h2>Expense Tracker</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const [displayForm, setDisplayForm] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString,
+    };
+    props.onAddExpense(expenseData);
+    setDisplayForm(false);
+  };
+
+  const buttonAddHandler = () => {
+    setDisplayForm(true);
+  };
+
+  const cancelButtonHandler = () => {
+    setDisplayForm(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      {!displayForm && (
+        <button type="button" onClick={buttonAddHandler}>
+          Add Expenses
+        </button>
+      )}
+      {displayForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelButtonHandler}
+        />
+      )}
     </div>
   );
 };
